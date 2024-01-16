9fb2b65efe605ec6ebca2248cbe05f5bf61641e4
Brian Nippert <briannippert@gmail.com>
2024-01-15 21:08:47 -0500

    Update Usabiliyt
    Added requirements.txt and added the ability to have multiple watched schools

--- run.py
+++ run.py
@@ -7,13 +7,13 @@ from email.mime.text import MIMEText
 import time
 load_dotenv()
 
-def check_school_closure():
+def check_school_closure(schoolName):
     url = 'https://www.wmur.com/weather/closings'
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
 
-    # Find the div for Sanborn Regional School District
-    school_div = soup.find('div', class_='weather-closings-data-item', attrs={'data-name': os.getenv("school_name")})
+    # Find the div for Given School
+    school_div = soup.find('div', class_='weather-closings-data-item', attrs={'data-name': schoolName})
 
     if school_div:
         status_div = school_div.find('div', class_='weather-closings-data-status')
@@ -21,30 +21,31 @@ def check_school_closure():
             return status_div.text.strip()
     return 'Status not found'
 
-def send_email(status_message):
+def send_email(status_message, school):
     sender = 'SCHOOL_ALERT@gmail.com'
-    receiver = 'briannippert@gmail.com;rachelnoland20@gmail.com'
-    subject = str(os.getenv("school_name")) + ' SNOW DAY!'
+    subject = school + ' SNOW DAY!'
 
     msg = MIMEText(status_message)
     msg['Subject'] = subject
     msg['From'] = sender
-    msg['To'] = receiver
+    msg['To'] = os.getenv("email_addresses")
     # Replace with your SMTP server details
     with smtplib.SMTP_SSL('smtp.gmail.com', 465) as server:
         server.login(os.getenv("smtp_username"), os.getenv("smtp_password"))
-        server.sendmail(sender, receiver, msg.as_string())
+        server.sendmail(sender, os.getenv("email_addresses"), msg.as_string())
         server.close()
 
 
 def main():
     while True:
-        status_message = check_school_closure()
-        if status_message and status_message != 'Status not found':
-            send_email(status_message)
-            print("Email sent with the following status: " + status_message)
-        else:
-            print("Sanborn Regional School District status not found or school is not closed.")
+        schools = str.split(os.getenv("school_names"),',')
+        for school in schools:
+            status_message = check_school_closure(school)
+            if status_message and status_message != 'Status not found':
+                send_email(status_message, school)
+                print("Email sent with the following status: " + status_message)
+            else:
+                print( school + ": status not found or school is not closed.")
 
         # Wait for 1 minute before checking again
         time.sleep(60)
